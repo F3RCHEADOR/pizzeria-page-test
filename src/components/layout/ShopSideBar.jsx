75c0e2b3bdb630bad64b3cdb149ab58a1b08fd8b
Fabian F <noobseca123@hotmail.com>
2025-04-12 11:08:23 -0500

    Inicializador de Nanostore en el sideBarShopping

--- src/components/layout/ShopSideBar.jsx
+++ src/components/layout/ShopSideBar.jsx
@@ -1,9 +1,14 @@
-
+import { useStore } from "@nanostores/react";
+import { isCartOpen } from "../../stores/cartStore";
 
 export default function ShopSideBar() {
-    return (
-        <aside className="fixed right-0 top-20 h-dvh border-l-2 w-56 bg-gray-100">
-           
-        </aside>
-    )
+  const $isCartOpen = useStore(isCartOpen);
+
+  return (
+    <aside
+      className={`fixed ${
+        $isCartOpen ? "right-0" : "-right-56"
+      } top-20 h-dvh border-l-2 w-56 transition-all bg-gray-100`}
+    ></aside>
+  );
 }
